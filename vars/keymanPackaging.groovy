96badce3fbd8cd689d4037a7bbeaeb19dcebb864
Eberhard Beilharz <eb1@sil.org>
2019-11-12 17:18:56 +0100

    [keyman-pkg] Debugging

--- vars/keymanPackaging.groovy
+++ vars/keymanPackaging.groovy
@@ -63,6 +63,25 @@ def call(body) {
       node('master') {
         stage('checkout source') {
           checkout scm
+
+          sh 'env'
+
+          echo '************************'
+          def changeLogSets = currentBuild.changeSets
+          for (int i = 0; i < changeLogSets.size(); i++) {
+              def entries = changeLogSets[i].items
+              for (int j = 0; j < entries.length; j++) {
+                  def entry = entries[j]
+                  echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+                  def files = new ArrayList(entry.affectedFiles)
+                  for (int k = 0; k < files.size(); k++) {
+                      def file = files[k]
+                      echo "  ${file.editType.name} ${file.path}"
+                  }
+              }
+          }
+          echo '************************'
+
           if (gitHub.isPRBuild()) {
             if (!utils.hasMatchingChangedFiles(pullRequest.files, changedFileRegex)) {
               echo "Skipping PR since it didn't change any Linux-related files"
