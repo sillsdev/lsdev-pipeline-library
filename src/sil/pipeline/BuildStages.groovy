c3956b9199939c02489b57f5eb573a161f0092d5
Eberhard Beilharz <eb1@sil.org>
2020-04-16 15:23:27 +0200

    Fix workaround for libgit2sharp bug
    Sometimes we also have to create a local master branch.
    
    libgit2sharp bug: https://github.com/libgit2/libgit2sharp/issues/1782
    GitVersion bug: https://github.com/GitTools/GitVersion/issues/2115

--- src/sil/pipeline/BuildStages.groovy
+++ src/sil/pipeline/BuildStages.groovy
@@ -45,7 +45,7 @@ def getWinBuildStage(String winNodeSpec, String winTool, Boolean uploadNuGet, St
           // Workaround for bug in libgit2sharp which causes a crash in GitVersion when local
           // branch doesn't exist yet.
           def gitbash = "C:\\Program Files (x86)\\Git\\git-bash"
-          def cmd = "for i in `git branch -a | grep remote | grep -v HEAD | grep -v master`; do git branch --track \${i#remotes/origin/} \$i 2> /dev/null || true ; done"
+          def cmd = "for i in `git branch -a | grep remote | grep -v HEAD`; do git branch --track \${i#remotes/origin/} \$i 2> /dev/null || true ; done"
           bat """
             "${gitbash}" -c "${cmd}"
             """
@@ -173,7 +173,7 @@ def getLinuxBuildStage(String linuxNodeSpec, String linuxTool, Boolean clean,
 
             // Workaround for bug in libgit2sharp which causes a crash in GitVersion when local
             // branch doesn't exist yet.
-            sh "for i in `${git} branch -a | grep remote | grep -v HEAD | grep -v master`; do ${git} branch --track \${i#remotes/origin/} \$i 2> /dev/null || true ; done"
+            sh "for i in `${git} branch -a | grep remote | grep -v HEAD`; do ${git} branch --track \${i#remotes/origin/} \$i 2> /dev/null || true ; done"
             sh "${gitversion}"
           }
 
