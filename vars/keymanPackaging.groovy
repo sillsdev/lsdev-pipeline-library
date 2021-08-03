ecb4550b682f182172666d3edb0db1180f283ae9
Eberhard Beilharz <eb1@sil.org>
2021-08-03 16:47:40 +0200

    [keyman-pkg] Refactor keymanPackaging
    This removes the additional keymanPackaging2 script and adds an
    additional parameter to keymanPackaging that allows to specify
    the packages to build, so this achieves the same.

--- vars/keymanPackaging.groovy
+++ vars/keymanPackaging.groovy
@@ -26,6 +26,7 @@ def call(body) {
 
   def distributionsToPackage = params.distributionsToPackage ?: supportedDistros
   def arches = params.arches ?: defaultArches
+  def packagesToBuild = params.packagesToBuild ?: ['keyman-keyboardprocessor', 'kmflcomp', 'libkmfl', 'ibus-kmfl', 'keyman-config', 'ibus-keyman']
 
   def isAlpha  = env.BRANCH_NAME == 'master'
   def isBeta   = env.BRANCH_NAME == 'beta'
@@ -181,7 +182,7 @@ def call(body) {
               ).trim()
           }
 
-          stash name: 'sourcetree', includes: 'linux/,resources/,common/,TIER.md,VERSION.md'
+          stash name: 'sourcetree', includes: 'linux/,debian/,resources/,common/,TIER.md,VERSION.md'
           stash name: 'packages', includes: 'results/*', allowEmpty: true
         }
       }
@@ -214,7 +215,7 @@ def call(body) {
           parallel dependencyTasks
 
           def tasks = [:]
-          for (p in ['keyman-keyboardprocessor', 'kmflcomp', 'libkmfl', 'ibus-kmfl', 'keyman-config', 'ibus-keyman']) {
+          for (p in packagesToBuild) {
             // don't inline this!
             def packageName = p
 
@@ -252,12 +253,15 @@ def call(body) {
                   unstash name: 'sourcetree'
 
                   switch (packageName) {
+                    case 'keyman':
+                      subDirName = ''
+                      break
                     case 'keyman-keyboardprocessor':
                       subDirName = fileExists('common/core/desktop') ?
-                        'common/core/desktop' : 'common/engine/keyboardprocessor'
+                        'common/core/desktop/' : 'common/engine/keyboardprocessor/'
                       break
                     default:
-                      subDirName = "linux/${packageName}"
+                      subDirName = fileExists("linux/legacy/${packageName}") ? "linux/legacy/${packageName}/" : "linux/${packageName}/"
                       break
                   }
 
@@ -265,7 +269,7 @@ def call(body) {
 cd linux
 ./scripts/jenkins.sh ${packageName} \$DEBSIGNKEY
 """
-                  stash name: "${packageName}-srcpkg", includes: "${subDirName}/${packageName}_*, ${subDirName}/debian/"
+                  stash name: "${packageName}-srcpkg", includes: "${subDirName}${packageName}_*, ${subDirName}debian/"
                 } /* stage */
               } /* node */
 
@@ -282,30 +286,23 @@ cd linux
 
                   node(binaryPackagerNode) {
                     stage("building ${packageName} (${dist}/${arch})") {
-                      if ((packageName == 'ibus-keyman' || packageName == 'keyman-keyboardprocessor') && dist == 'xenial') {
+                      echo "Building ${packageName} (${dist}/${arch})"
 
-                        // The build should work on all dists, but currently it's failing on xenial.
-                        // 2020-03-18 For now we don't build these two packages on Xenial and
-                        // don't report them.
-                        // org.jenkinsci.plugins.pipeline.modeldefinition.Utils.markStageSkippedForConditional(STAGE_NAME)
-                      } else {
-                        echo "Building ${packageName} (${dist}/${arch})"
+                      def retryCount = 0
+                      retry(3) {
+                        if (retryCount > 0) {
+                          // we're retrying a second or third time. Wait for a minute - the
+                          // failure might have been caused by some server updates or internet
+                          // troubles which might work again after a while.
+                          sleep(60 /*seconds*/)
+                        }
+                        retryCount++
+                        sh 'rm -rf *'
 
-                        def retryCount = 0
-                        retry(3) {
-                          if (retryCount > 0) {
-                            // we're retrying a second or third time. Wait for a minute - the
-                            // failure might have been caused by some server updates or internet
-                            // troubles which might work again after a while.
-                            sleep(60 /*seconds*/)
-                          }
-                          retryCount++
-                          sh 'rm -rf *'
+                        unstash name: "${packageName}-srcpkg"
 
-                          unstash name: "${packageName}-srcpkg"
-
-                          def buildResult = sh(
-                            script: """#!/bin/bash
+                        def buildResult = sh(
+                          script: """#!/bin/bash
 # Check that we actually want to build this combination!
 echo "dist=${dist}; DistributionsToPackage=\$DistributionsToPackage"
 if [[ "\$DistributionsToPackage" != *${dist}* ]] || [[ "\$ArchesToPackage" != *${arch}* ]]; then
@@ -314,30 +311,29 @@ exit 50
 fi
 
 basedir=\$(pwd)
-cd ${subDirName}
+[ "${subDirName}" != "" ] && cd ${subDirName}
 
 \$HOME/ci-builder-scripts/bash/build-package --dists "${dist}" --arches "${arch}" --main-package-name "${fullPackageName}" --supported-distros "${supportedDistros}" --debkeyid \$DEBSIGNKEY --build-in-place --no-upload ${buildPackageArgs}
 """,
-                            returnStatus: true)
-                          if (buildResult == 50) {
-                            // buildResult 50 means that we don't have to build for this
-                            // architecture (i386) because the architecture is listed as
-                            // 'all' and so gets build when we build for amd64.
-                            org.jenkinsci.plugins.pipeline.modeldefinition.Utils.markStageSkippedForConditional(STAGE_NAME)
-                          } else if (buildResult != 0) {
-                            error "Package build of ${packageName} (${dist}/${arch}) failed in the previous step (exit code ${buildResult})"
-                          } else {
-                            if (!gitHub.isPRBuild()) {
-                              lock('packages') {
-                                unstash name: 'packages'
-                                stash name: 'packages', includes: 'results/*'
-                              }
+                          returnStatus: true)
+                        if (buildResult == 50) {
+                          // buildResult 50 means that we don't have to build for this
+                          // architecture (i386) because the architecture is listed as
+                          // 'all' and so gets build when we build for amd64.
+                          org.jenkinsci.plugins.pipeline.modeldefinition.Utils.markStageSkippedForConditional(STAGE_NAME)
+                        } else if (buildResult != 0) {
+                          error "Package build of ${packageName} (${dist}/${arch}) failed in the previous step (exit code ${buildResult})"
+                        } else {
+                          if (!gitHub.isPRBuild()) {
+                            lock('packages') {
+                              unstash name: 'packages'
+                              stash name: 'packages', includes: 'results/*'
                             }
-                            archiveArtifacts artifacts: 'results/*'
                           }
-                          sh 'rm -rf *'
-                        } // retry
-                      } /* if/else */
+                          archiveArtifacts artifacts: 'results/*'
+                        }
+                        sh 'rm -rf *'
+                      } // retry
                     } /* stage */
                   } /* node */
                 } /* tasks */
